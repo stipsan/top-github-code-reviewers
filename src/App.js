25331b60a216778614e7a6b3e3eda39b924f6e91
Stian Didriksen <stipsan@gmail.com>
2017-07-28 00:33:54 +0200

    prettier

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ const height = 640
 
 class App extends Component {
   render() {
-    const {repository, loading} = this.props.data
+    const { repository, loading } = this.props.data
 
-    if(loading) {
+    if (loading) {
       return false
     }
     const data = JSON.stringify(calcScores(repository.pullRequests), null, 2)
 
-    const {topReviewers} = calcScores(repository.pullRequests)
+    const { topReviewers } = calcScores(repository.pullRequests)
 
     return (
       <div className="App">
@@ -28,43 +28,102 @@ class App extends Component {
           </pre>
         </div>
         <p className="App-intro">
-          <svg width="100%" height="100%" viewBox={`0 0 ${width} ${height}`} version="1.1" xmlns="http://www.w3.org/2000/svg">
-          <title>
-            Artboard
-          </title>
-          <g fill="none" fillRule="evenodd">
-            {topReviewers.map((reviewer, i) => {
-              return <g key={reviewer.author.login}>
-              <path d={`M 0 ${i * 64}`} d="M1.5 163.5h639.003" stroke="#EAECEF" stroke-linecap="square"/>
+          <svg
+            width="100%"
+            height="100%"
+            viewBox={`0 0 ${width} ${height}`}
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Artboard</title>
+            <g fill="none" fillRule="evenodd">
+              {topReviewers.map((reviewer, i) => {
+                return (
+                  <g key={reviewer.author.login}>
+                    <path
+                      d={`M 0 ${i * 64}`}
+                      d="M1.5 163.5h639.003"
+                      stroke="#EAECEF"
+                      stroke-linecap="square"
+                    />
+                    <text fontSize="22" fill="#FFF">
+                      <tspan x="20" y="120">
+                        {reviewer.author.login}
+                      </tspan>
+                    </text>
+                  </g>
+                )
+              })}
+              <rect
+                fill="url(#b)"
+                x="79"
+                y="97"
+                width="51"
+                height="47"
+                rx="8"
+              />
+              <path
+                d="M1.5 163.5h639.003"
+                stroke="#EAECEF"
+                strokeLinecap="square"
+              />
               <text fontSize="22" fill="#FFF">
-                <tspan x="20" y="120">{reviewer.author.login}</tspan>
+                <tspan x="20" y="120">
+                  🥇
+                </tspan>
               </text>
-            </g>})}
-            <rect fill="url(#b)" x="79" y="97" width="51" height="47" rx="8"/>
-            <path d="M1.5 163.5h639.003" stroke="#EAECEF" strokeLinecap="square"/>
-            <text fontSize="22" fill="#FFF">
-              <tspan x="20" y="120">🥇</tspan>
-            </text>
-            <text fontSize="22" fill="#000">
-              <tspan x="13" y="223">🥈</tspan>
-            </text>
-            <text fontSize="22" fill="#000">
-              <tspan x="13" y="308">🥉</tspan>
-            </text>
-            <text font-family="Helvetica" font-size="22" letter-spacing=".81" fill="#586069">
-              <tspan x="147" y="120">Winner</tspan>
-            </text>
-            <text font-family="Helvetica" font-size="22" letter-spacing=".81" fill="#586069">
-              <tspan x="554" y="126">10</tspan>
-            </text>
-            <text font-family="Helvetica" font-size="22" letter-spacing=".81" fill="#586069">
-              <tspan x="38" y="37">Top Code Reviewers</tspan>
-            </text>
-            <text font-family="Helvetica" font-size="22" letter-spacing=".81" fill="#586069">
-              <tspan x="536" y="26">Score</tspan>
-            </text>
-          </g>
-        </svg>
+              <text fontSize="22" fill="#000">
+                <tspan x="13" y="223">
+                  🥈
+                </tspan>
+              </text>
+              <text fontSize="22" fill="#000">
+                <tspan x="13" y="308">
+                  🥉
+                </tspan>
+              </text>
+              <text
+                font-family="Helvetica"
+                font-size="22"
+                letter-spacing=".81"
+                fill="#586069"
+              >
+                <tspan x="147" y="120">
+                  Winner
+                </tspan>
+              </text>
+              <text
+                font-family="Helvetica"
+                font-size="22"
+                letter-spacing=".81"
+                fill="#586069"
+              >
+                <tspan x="554" y="126">
+                  10
+                </tspan>
+              </text>
+              <text
+                font-family="Helvetica"
+                font-size="22"
+                letter-spacing=".81"
+                fill="#586069"
+              >
+                <tspan x="38" y="37">
+                  Top Code Reviewers
+                </tspan>
+              </text>
+              <text
+                font-family="Helvetica"
+                font-size="22"
+                letter-spacing=".81"
+                fill="#586069"
+              >
+                <tspan x="536" y="26">
+                  Score
+                </tspan>
+              </text>
+            </g>
+          </svg>
         </p>
       </div>
     )
