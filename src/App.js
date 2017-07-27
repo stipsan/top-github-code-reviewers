673fda4cc19a7c546b5dd9b4737682340ff5f2f5
Stian Didriksen <stipsan@gmail.com>
2017-07-28 00:33:45 +0200

    working on rendering the svg

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { gql, graphql } from 'react-apollo'
 import calcScores from './utils/calc-scores'
 import Test from './Test'
 
+const width = 640
+const height = 640
+
 class App extends Component {
   render() {
     const {repository, loading} = this.props.data
@@ -12,6 +15,8 @@ class App extends Component {
       return false
     }
     const data = JSON.stringify(calcScores(repository.pullRequests), null, 2)
+
+    const {topReviewers} = calcScores(repository.pullRequests)
 
     return (
       <div className="App">
@@ -23,7 +28,43 @@ class App extends Component {
           </pre>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <svg width="100%" height="100%" viewBox={`0 0 ${width} ${height}`} version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <title>
+            Artboard
+          </title>
+          <g fill="none" fillRule="evenodd">
+            {topReviewers.map((reviewer, i) => {
+              return <g key={reviewer.author.login}>
+              <path d={`M 0 ${i * 64}`} d="M1.5 163.5h639.003" stroke="#EAECEF" stroke-linecap="square"/>
+              <text fontSize="22" fill="#FFF">
+                <tspan x="20" y="120">{reviewer.author.login}</tspan>
+              </text>
+            </g>})}
+            <rect fill="url(#b)" x="79" y="97" width="51" height="47" rx="8"/>
+            <path d="M1.5 163.5h639.003" stroke="#EAECEF" strokeLinecap="square"/>
+            <text fontSize="22" fill="#FFF">
+              <tspan x="20" y="120">🥇</tspan>
+            </text>
+            <text fontSize="22" fill="#000">
+              <tspan x="13" y="223">🥈</tspan>
+            </text>
+            <text fontSize="22" fill="#000">
+              <tspan x="13" y="308">🥉</tspan>
+            </text>
+            <text font-family="Helvetica" font-size="22" letter-spacing=".81" fill="#586069">
+              <tspan x="147" y="120">Winner</tspan>
+            </text>
+            <text font-family="Helvetica" font-size="22" letter-spacing=".81" fill="#586069">
+              <tspan x="554" y="126">10</tspan>
+            </text>
+            <text font-family="Helvetica" font-size="22" letter-spacing=".81" fill="#586069">
+              <tspan x="38" y="37">Top Code Reviewers</tspan>
+            </text>
+            <text font-family="Helvetica" font-size="22" letter-spacing=".81" fill="#586069">
+              <tspan x="536" y="26">Score</tspan>
+            </text>
+          </g>
+        </svg>
         </p>
       </div>
     )
