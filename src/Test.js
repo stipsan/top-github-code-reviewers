de0d63d96516628ecd08737d64175104c9d967c6
Stian Didriksen <stipsan@gmail.com>
2017-07-29 23:06:52 +0200

    rendering works

--- src/Test.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export default () => ['Test!', <b>'Array!'</b>]
--- ssr-svg.js
+++ ssr-svg.js
@@ -1,16 +1,15 @@
 import ReactDOMServer from 'react-dom/server'
 
-import {
-  ApolloClient,
-  createNetworkInterface,
-  ApolloProvider,
-  getDataFromTree,
-} from 'react-apollo'
+import { ApolloProvider, getDataFromTree } from 'react-apollo'
 
 import App from './src/App'
 import createClient from './src/client'
 
-export default (fallbackToken = process.env.GITHUB_TOKEN) => (req, res) => {
+export default (fallbackToken = process.env.GITHUB_TOKEN) => (
+  req,
+  res,
+  next
+) => {
   // This example uses React Router, although it should work equally well with other
   // routers that support SSR
 
@@ -18,21 +17,21 @@ export default (fallbackToken = process.env.GITHUB_TOKEN) => (req, res) => {
 
   const client = createClient(access_token, { ssrMode: true })
 
-  console.log('constructing client', client)
-
   const app = (
     <ApolloProvider client={client}>
       <App owner={owner} name={name} />
     </ApolloProvider>
   )
 
-  console.log('about to render')
+  // Run all graphql queries
+  getDataFromTree(app)
+    .then(() => {
+      // We are ready to render for real
+      const html = ReactDOMServer.renderToStaticMarkup(app)
 
-  getDataFromTree(app).then(() => {
-    // We are ready to render for real
-    console.log('ready to render')
-    res.status(200)
-    res.send(`<!doctype html>\n${ReactDOMServer.renderToStaticMarkup(app)}`)
-    res.end()
-  })
+      res.status(200)
+      res.send(`<!doctype html>\n${html}`)
+      res.end()
+    })
+    .catch(next)
 }
