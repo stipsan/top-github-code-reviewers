ffbd8cc45c26c71d1b8d1a9cb759c2bad3b6eb32
Stian Didriksen <stipsan@gmail.com>
2017-07-31 00:38:27 +0200

    styling

--- components/ErrorSvg.js
+++ components/ErrorSvg.js
@@ -1,12 +1,13 @@
 export default ({ error }) =>
   <svg
-    width="100%"
-    height="100%"
+    width="500px"
+    height="40px"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 500 40"
     xmlnsXlink="http://www.w3.org/1999/xlink"
   >
-    <g fontFamily="Helvetica" fontSize="22">
+    <g fontFamily="Helvetica" fontSize="12">
       <text x={0} y={28}>
         {error}
       </text>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,74 @@
 import { Component } from 'react'
 import styled from 'styled-components'
+import { Flex, Box } from 'grid-styled'
 
 import Page from '../components/Page'
 import { setToken } from '../src/utils/github-token'
 import App from '../src/App'
 import AppContainer from '../components/AppContainer'
 
-const Input = styled.input`border-radius: 3px;`
+const primaryColor = '#344a5f'
+
+const Input = styled.input`
+  border-radius: 4px;
+  border: 2px solid ${primaryColor};
+  color: ${primaryColor};
+  display: block;
+  width: 100%;
+  margin: 3px;
+  padding: 5px;
+`
+
+const Form = styled.form`
+  & > div {
+    position: -webkit-sticky;
+    position: sticky;
+    top: 10px;
+    background: white;
+    border: 2px solid #f6f8fa;
+    border-radius: 4px;
+  }
+`
+
+const SvgLink = styled.a`
+  display: block;
+  background: white;
+  border: 2px solid #f6f8fa;
+  border-radius: 4px;
+
+  img {
+    width: 100%;
+  }
+`
+
+const Submit = styled.input`
+  display: block;
+  color: white;
+  background: #2ecc71;
+  box-sizing: content-box;
+  margin: 3px;
+  margin-top: 15px;
+  padding: 5px;
+  border: 2px solid transparent;
+  border-radius: 4px;
+  width: 100%;
+
+  :invalid & {
+    pointer-events: none;
+  }
+`
+
+const Header = styled.h2`color: ${primaryColor};`
+
+const PreviewMarkdown = styled.textarea`
+  display: block;
+  border-radius: 4px;
+  background: rgba(52, 74, 95, 0.05);
+  border: none;
+  padding: 10px;
+  min-height: 128px;
+  width: 100%;
+`
 
 export default class Index extends Component {
   state = {
@@ -39,32 +101,49 @@ export default class Index extends Component {
       : `/${owner}/${name}`
     return (
       <Page>
-        Usage instructions will appear here, soonish.
-        <form>
-          <Input
-            name="owner"
-            type="text"
-            value={owner}
-            onChange={this.handleChange}
-          />
-          <Input
-            name="name"
-            type="text"
-            value={name}
-            onChange={this.handleChange}
-          />
-          <Input
-            type="text"
-            value={access_token}
-            onChange={this.handleTokenChange}
-          />
-          <input type="submit" />
-        </form>
-        {dirty
-          ? <AppContainer key={access_token} owner={owner} name={name} />
-          : <a href={`${href}.svg`} target="_blank">
-              <img src={`${href}.svg`} />
-            </a>}
+        <Flex wrap align="flex-start" justify="space-around" pt={3} pb={3}>
+          <Box pb={1}>
+            <Form>
+              <Flex column align="center" px={3} pb={2}>
+                <Header>Generate SVG</Header>
+                <Input
+                  required
+                  name="owner"
+                  type="text"
+                  value={owner}
+                  onChange={this.handleChange}
+                />
+                <Input
+                  required
+                  name="name"
+                  type="text"
+                  value={name}
+                  onChange={this.handleChange}
+                />
+                <Input
+                  type="text"
+                  value={access_token}
+                  onChange={this.handleTokenChange}
+                  placeholder="Personal Access Token"
+                  title="Only needed for private repositories"
+                />
+                <Submit type="submit" value="Preview SVG" />
+                <Header>Markdown</Header>
+                <PreviewMarkdown
+                  readonly
+                  value={`[![Top Code Reviewers](https://top-github-code-reviewers.stipsan.io/${href}.svg)](https://top-github-code-reviewers.stipsan.io/${href})`}
+                />
+              </Flex>
+            </Form>
+          </Box>
+          <Box>
+            {dirty
+              ? <AppContainer key={access_token} owner={owner} name={name} />
+              : <SvgLink href={`${href}.svg`} target="_blank">
+                  <img src={`${href}.svg`} />
+                </SvgLink>}
+          </Box>
+        </Flex>
       </Page>
     )
   }
